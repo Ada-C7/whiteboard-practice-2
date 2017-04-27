15b027ae76779a16fc3c49e7745e32ad05f4c382
sai samant <saisamant@gmail.com>
2017-04-27 08:53:14 -0700

    solutions added

---
+++ solutions.rb
@@ -0,0 +1,48 @@
+# ## Problem #1
+# Write a method `mirror` that returns a **new array** doubling the size of a list of integers by appending the mirror image of the original sequence to the end of the list. The mirror image is the same sequence of values in reverse order. For example, if a variable called list stores this sequence of values:
+#
+# [1, 3, 2, 7]
+# and we make the following call:
+#
+# mirror(list)
+# then it should return the following values after the call:
+#
+# [1, 3, 2, 7, 7, 2, 3, 1]
+# Notice that it has been doubled in size by having the original sequence appearing in reverse order at the end of the list. You may not make assumptions about how many elements are in the list.
+
+def mirror list
+  count = list.length
+  index = count-1
+  count.times do
+    mirror_var = list[index]
+    list << mirror_var
+    index -= 1
+  end
+  return list
+end
+
+list =  [1, 5, 7, 3]
+puts mirror(list)
+
+# ## Problem #2
+# Write a method `switchPairs` that switches the order of values in an Array of strings in a pairwise fashion. Your method should switch the order of the first two values, then switch the order of the next two, switch the order of the next two, and so on. **This switch should happen _in place_, meaning no new array should be created.** For example, if the list initially stores these values: ["four", "score", "and", "seven", "years", "ago"] your method should switch the first pair, "four", "score", the second pair, "and", "seven", and the third pair, "years", "ago", to yield this list: ["score", "four", "seven", "and", "ago", "years"]
+#
+# If there are an odd number of values in the list, the final element is not moved. For example, if the original list had been: ["to", "be", "or", "not", "to", "be", "hamlet"] It would again switch pairs of values, but the final value, "hamlet" would not be moved, yielding this list: ["be", "to", "not", "or", "be", "to", "hamlet"]
+
+def switch_pairs strings
+  counter = 0
+  while counter <= strings.length - 2
+    temp = strings[counter]
+    strings[counter] = strings[counter+1]
+    strings[counter+1] = temp
+    counter += 2
+  end
+
+  return strings
+end
+
+strings1 = ["four", "score", "and", "seven", "years", "ago"]
+strings2 = ["to", "be", "or", "not", "to", "be", "hamlet"]
+
+puts switch_pairs(strings1)
+puts switch_pairs(strings2)
